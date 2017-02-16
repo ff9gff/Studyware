b6b77352d83ead8c3421a52f652316dc70fac9d7
ff9gf <ff9gf@DESKTOP-CQ54QBL>
2017-02-16 13:16:08 +0900

    2017/02/16 테스트중

---
+++ Studyware/src/main/java/edu/spring/studyware/controller/admin/RegisterController.java
@@ -0,0 +1,15 @@
+package edu.spring.studyware.controller.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class RegisterController {
+
+	private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
+	
+}
---
+++ Studyware/src/main/java/edu/spring/studyware/controller/register/AdminController.java
@@ -0,0 +1,15 @@
+package edu.spring.studyware.controller.register;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class AdminController {
+
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+	
+}
