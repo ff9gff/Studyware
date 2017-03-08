c001e0e4f1c12d0e52c6f17536ce6d3fd2c8a65d
koyuna <koyuna@yuna>
2017-03-08 10:25:26 +0900

    2017/03/08 마이페이지

--- Studyware/src/main/java/edu/spring/studyware/member/controller/MypageController.java
+++ Studyware/src/main/java/edu/spring/studyware/member/controller/MypageController.java
@@ -3,6 +3,8 @@ package edu.spring.studyware.member.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
@@ -12,4 +14,8 @@ public class MypageController {
 
 	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
 	
+	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
+	public String mypage(){
+		return "mypage/my_page";
+	}// end popupMsg
 }
---
+++ Studyware/src/main/java/edu/spring/studyware/register/controller/ProgressRESTController.java
@@ -0,0 +1,16 @@
+package edu.spring.studyware.register.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/mypage/progress")
+public class ProgressRESTController {
+
+	// 해당 진행상황 리스트 읽기
+	//@RequestMapping(value="/all/{no}", method = RequestMethod.GET)
+	//public ResponseEntity<List<>>
+}// end class
